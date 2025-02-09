0afa113bf31617a39ae0d8df68f213fa4f5a6497
Bruno Bucci <bruno@brunobucci.com>
2025-02-09 13:02:03 -0300

    Ajustes

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,8 @@
 # Use a imagem oficial do MongoDB
 FROM mongo:latest
 
-# Crie um diretório para os scripts de inicialização
-COPY ./init-mongo.js /docker-entrypoint-initdb.d/init-mongo.js
-
-# Declare o diretório de dados como persistente
-VOLUME /data/db
-
 # Exponha a porta padrão do MongoDB
 EXPOSE 27018
 
 # Comando padrão para rodar o MongoDB
-CMD ["mongod"]
+CMD ["mongod"] 
--- init-mongo.js
+++ init-mongo.js
@@ -1,6 +1,12 @@
 db = db.getSiblingDB('Hackathon'); // Cria ou acessa o banco 
 
-// Criação da coleção "video" sem inserir documentos
+// Criação da coleção "usuario" sem inserir documentos
 db.createCollection("video");
 
-print("Banco de dados e coleções configurados com sucesso!");
+db.createUser({
+    user: "soat7grupo5",
+    pwd: "Hc9YCOqLHPsKvf7e",
+    roles: [{ role: "root", db: "Hackathon" }]
+});
+
+print("Banco de dados e coleções configurados com sucesso!");  
